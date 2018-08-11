8aea5c221c162e114baf53b5ae2b0e86c075e9c4
xeeraki <shafai86@gmail.com>
2018-08-11 14:23:04 +0200

    quick sort

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -1,27 +1,59 @@
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
 import edu.princeton.cs.introcs.StdRandom;
 
 public class QuickSort {
+    public static void main(String[] args){
+        String[] a  = StdIn.readAllStrings();
+        sort(a);
+        show(a);
+    }
 
     public static void sort(Comparable[] a){
         StdRandom.shuffle(a);
         sort(a,0,a.length-1);
     }
 
-    private static void sort(Comparable[] a, int lo, int hi){
-        if(hi <= lo) return;
-        int partition(a,lo,hi);
-        sort(a,lo,j-1);
-        sort(a,j+1,hi);
+    private static boolean less(Comparable v , Comparable w){
+        return v.compareTo(w) < 0;
     }
+
+
 
     private static int partition(Comparable[] a, int lo, int hi){
         int i = lo, j = hi+1;
         Comparable v = a[lo];
 
         while(true){
-            while(less(a[++i]))
+            while(less(a[++i] ,v)) if(i == hi)break;
+            while(less(v, a[--j])) if(j == lo)break;
+            if(i >= j) break;
+            exch(a,i,j);
         }
+        exch(a,lo,j);
+        return j;
 
 
     }
+
+    private static void sort(Comparable[] a, int lo, int hi){
+        if(hi <= lo) return; // if just one  element in the array
+        int j = partition(a,lo,hi);
+        sort(a,lo,j-1);
+        sort(a,j+1,hi);
+    }
+
+
+    private static void exch(Comparable[] a , int i , int j){
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    private static void show(String[] a) {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + "");
+
+        StdOut.println();
+    }
+
 }
