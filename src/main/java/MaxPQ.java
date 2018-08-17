2f73df475ca08c614223003069d41a0fddaea712
xeeraki <shafai86@gmail.com>
2018-08-17 15:52:04 +0200

    heap ordered priority queue

--- src/main/java/MaxPQ.java
+++ src/main/java/MaxPQ.java
@@ -1,48 +1,30 @@
 public class MaxPQ<Key extends Comparable<Key>> {
-    private int N;
+    private int N = 0;
     private Node first;
+    private Key pq[];
 
     private class Node{
         Key key;
         Node next;
     }
 
-    public MaxPQ(){
-
-    }
-    public MaxPQ(int max,int capacity){
-
+    public MaxPQ(int maxN){
+        pq = (Key[]) new Comparable[maxN+1];
     }
 
-    public MaxPQ( Key[] a){
-
+    public void insert(Key v){
+        pq[++N] = v;
+        swim(N);
     }
 
-
-    public void insert(Key key){
-        Node oldfirst = first;
-        first = new Node();
-        first.key = key;
-        first.next = oldfirst;
-        N++;
-
-    }
-
-    public  Key delMax(Key max){
-
-
+    public  Key delMax(){
+        Key max = pq[1];
+        exch(1,N--);
+        pq[N+1] = null;
+        sink(1);
         return max;
     }
 
-    public Key max(Key max){
-        Key maxi = key[i];
-        for(int i = 0; i < key.length; i++){
-
-            if(less(maxi,max))
-                maxi = max;
-        }
-        return max;
-    }
 
     public boolean isEmpty(){
         return first == null;
@@ -52,7 +34,31 @@ public class MaxPQ<Key extends Comparable<Key>> {
         return N;
     }
 
-    public boolean less(Key v, Key w){
+    public boolean less(Comparable v, Comparable w){
         return v.compareTo(w)<0;
+    }
+    public void swim(int k){
+
+            while(k > 1 && less(k/2,k)){
+                exch(k/2,k);
+            k=k/2;
+        }
+    }
+
+    public  void exch(int i, int j){
+        Key temp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = temp;
+    }
+
+
+    public void sink(int k){
+        while(2*k <= N) {
+            int j = 2 * k;
+            if (k > 1 && less(j, j + 1)) j++;
+            if (!less(k, j)) break;
+            exch(k / 2, k);
+            k = j;
+        }
     }
 }
