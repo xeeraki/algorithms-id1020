ef0b553dd6939aa7169aaf52df593c80e3a95777
xeeraki <shafai86@gmail.com>
2018-08-19 18:34:17 +0200

    heap sort

--- src/main/java/heapSort.java
+++ src/main/java/heapSort.java
@@ -3,21 +3,15 @@ public class heapSort <Key extends Comparable <Key>>{
     private int N = 0;
     private Key pq[];
 
-    public heapSort(int maxN){
-        pq = (Key[]) new Comparable[maxN+1];
+    public static void sort(Comparable[] pq){
+    int N = pq.length;
+    for(int k = N/2; k >= 1; k--)
+        sink(pq,k,N);
+    while(N > 1){
+        exch(pq,1,N);
+        sink(pq,1,--N);
     }
 
-    public void insert(Key v){
-        pq[++N] = v;
-        swim(N);
-    }
-
-    public  Key delMax(){
-        Key max = pq[1];
-        exch(1,N--);
-        pq[N+1] = null;
-        sink(1);
-        return max;
     }
 
     public boolean isEmpty(){
@@ -28,30 +22,30 @@ public class heapSort <Key extends Comparable <Key>>{
         return N;
     }
 
-    private boolean less(int i, int j){
+    private static boolean less(Comparable[] pq,int i, int j){
         return pq[i].compareTo(pq[j])<0;
     }
-    public void swim(int k){
+    public void swim(Comparable[] pq,int k,int N){
 
-        while(k > 1 && less(k/2,k)){
-            exch(k/2,k);
+        while(k > 1 && less(pq,k/2,k)){
+            exch(pq,k/2,k);
             k=k/2;
         }
     }
 
-    public  void exch(int i, int j){
-        Key temp = pq[i];
+    public  static void exch(Comparable[] pq,int i, int j){
+        Comparable temp = pq[i];
         pq[i] = pq[j];
         pq[j] = temp;
     }
 
 
-    public void sink(int k){
+    public static void sink(Comparable[] pq,int k, int N){
         while(2*k <= N) {
             int j = 2 * k;
-            if (k > 1 && less(j, j + 1)) j++;
-            if (!less(k, j)) break;
-            exch(k / 2, k);
+            if (k > 1 && less(pq,j, j + 1)) j++;
+            if (!less(pq,k, j)) break;
+            exch(pq,k / 2, k);
             k = j;
         }
     }
