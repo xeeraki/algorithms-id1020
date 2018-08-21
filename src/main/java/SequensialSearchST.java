e3dbb94d5300853028cfbb51a15c9dd714dc118e
xeeraki <shafai86@gmail.com>
2018-08-21 23:06:34 +0200

    some new methods added

--- src/main/java/SequensialSearchST.java
+++ src/main/java/SequensialSearchST.java
@@ -1,6 +1,14 @@
-public class SequensialSearchST<Key , Value> {
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class SequentialSearchST<Key , Value> {
 
     private Node first;
+    private Node last;
     private int N;
 
     private class Node{
@@ -21,15 +29,35 @@ public class SequensialSearchST<Key , Value> {
     public int size(){
         return N;
     }
-
-    public Iterable<Key> keys(){
-
+    public void enqueue(Key key){
+        Node oldLast = last;
+        last = new Node();
+        last.key = key;
+        last.next = null;
+        if(isEmpty()) first = last;
+        else oldLast.next = last;
+        N++;
     }
 
 
+    public boolean isEmpty(){
+        return first == null;
+    }
     //lazy version of delete
     public void delete(Key key){
-        put(key,null);
+        //put(key,null);
+        first = delete(first,key);
+    }
+
+    private Node delete(Node x, Key key){
+        if(x == null)
+            return null;
+        if(key.equals(x.key)) {
+            N--;
+            return x.next;
+        }
+        x.next = delete(x.next,key);
+        return x;
     }
 
     public boolean contains(Key key){
@@ -52,5 +80,24 @@ public class SequensialSearchST<Key , Value> {
                 x.val = val;
             return;}
         first = new Node(key , val ,first);
+        N++;
+    }
+
+    public Iterable<Key> keys(){
+        Queue<Key> queue = new Queue<Key>();
+        for(Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+
+        return queue;
+    }
+    public static void main(String[] args){
+        SequentialSearchST<String , Integer> st = new SequentialSearchST<>();
+        for(int i = 0; !StdIn.isEmpty(); i++){
+            String key = StdIn.readString();
+            st.put(key,i);
+        }
+
+        for(String s : st.keys())
+            StdOut.println(s + " " + st.get(s));
     }
 }
