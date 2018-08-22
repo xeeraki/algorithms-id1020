98e976c3f6e71b9988b4bbd0b9b66840f98a0762
xeeraki <shafai86@gmail.com>
2018-08-22 23:20:37 +0200

    binary search tree

--- src/main/java/binarySearchST.java
+++ src/main/java/binarySearchST.java
@@ -1,14 +1,33 @@
+import static edu.princeton.cs.introcs.BinaryStdIn.isEmpty;
+
 public class binarySearchST<Key extends Comparable<Key> , Value> {
 
     private Key[] keys;
-    private Value[] val;
+    private Value[] vals;
     private int N;
 
 
     public binarySearchST(int capacity){
         keys = (Key[]) new Comparable[capacity];
-        val = (Value[])new Comparable[capacity];
+        vals = (Value[])new Object[capacity];
 
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public Value get(Key key){
+        if(isEmpty()) return null;
+        int i = rank(key);
+        if(i < N && keys[i].compareTo(key)== 0)
+            return vals[i];
+        else
+            return null;
+    }
+
+    private int rank(Key key) {
+        
     }
 
 }
