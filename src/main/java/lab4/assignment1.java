285629b3bf80f3dce0eae8d4ff0373c162a95189
xeeraki <shafai86@gmail.com>
2018-10-08 09:34:28 +0200

    DFS search

--- src/main/java/lab4/assignment1.java
+++ src/main/java/lab4/assignment1.java
@@ -1,6 +1,34 @@
 package lab4;
 
+import edu.princeton.cs.algs4.Graph;
+
 public class assignment1 {
+    private boolean marked[];
+    private int count;
 
 
+
+    public assignment1(Graph g, int s){
+        marked = new boolean[g.V()];
+        dfs(g,s);
+
+    }
+
+    private void dfs(Graph g, int v) {
+
+        marked[v] = true;
+        count++;
+        for(int w : g.adj(v)){
+            if(!marked[w])
+                dfs(g,w);
+        }
+    }
+
+    public boolean marked(int w) {
+        return marked[w];
+    }
+
+    public int count(){
+        return count;
+    }
 }
